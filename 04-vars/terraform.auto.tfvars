051181a4d976f62298100f5c3ed0666e85fea309
iliyastb <skiliyas300@gmail.com>
2023-09-19 21:32:34 +0530

    update

---
+++ 04-vars/terraform.auto.tfvars
@@ -0,0 +1,1 @@
+tfvars = "Hello from tfvars auto"
--- 04-vars/vars.tf
+++ 04-vars/vars.tf
@@ -1,7 +1,12 @@
+# default variable
 variable "sample" {
   default = 10
 }
-
 output "sample" {
   value = var.sample
+}
+# tfvars variable
+variable "tfvars" {}
+output "tfvars" {
+  value = var.tfvars
 }
