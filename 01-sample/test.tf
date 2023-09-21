2ea8c4704143b889eba580e87d2f3945c75749c3
iliyastb <skiliyas300@gmail.com>
2023-09-21 21:10:11 +0530

    update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,3 +1,16 @@
 # This is the testing of the tf code
 resource "null_resource" "null" {
 }
+
+variable "sample5" {
+  default = {
+    fruits = {
+      apple   = 5
+      oranges = 5
+    }
+  }
+}
+
+output "sample5" {
+  value = var.sample5.value.fruits[0]
+}
