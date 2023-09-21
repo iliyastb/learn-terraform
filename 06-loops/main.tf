64e085f5e8b422b6b33117a0c116ea96f63600ca
iliyastb <skiliyas300@gmail.com>
2023-09-21 20:48:33 +0530

    update

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -1,8 +1,17 @@
+# count
 variable "loop1" {
-  default = loop
+  default = 3
 }
 
 resource "null_resource" "nothing" {
   count = var.loop1
 }
 
+#length
+variable "loop2" {
+  default = ["orange", "banana"]
+}
+
+resource "null_resource" "nothing" {
+  count = length(var.loop2)
+}
