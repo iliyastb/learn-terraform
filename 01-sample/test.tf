c6dd2d5a8bc1e7f9aad4a0a6633131c982ebe29f
iliyastb <skiliyas300@gmail.com>
2023-09-21 21:42:14 +0530

    update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,17 +1,3 @@
 # This is the testing of the tf code
 resource "null_resource" "null" {
 }
-
-variable "sample" {
-  default = {
-    fruits = {
-      apple   = 5
-      orange = 5
-      banana = false
-    }
-  }
-}
-
-output "sample" {
-  value = var.sample.fruits.banana
-}
--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -7,11 +7,29 @@ resource "null_resource" "nothing1" {
   count = var.loop1
 }
 
-#length
+# length
 variable "loop2" {
   default = ["orange", "banana"]
 }
 
 resource "null_resource" "nothing2" {
   count = length(var.loop2)
+}
+
+# map
+variable "loop3" {
+  default = {
+    apple = {
+      name = "iliyas"
+      count = 2
+    }
+    banana = {
+      name = "purna"
+      count = 3
+    }
+  }
+}
+
+resource "null_resource" "loop3" {
+  for_each = var.loop3
 }
