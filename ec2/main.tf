117941c753fef41e0588a4dc4e550f9dca84f96b
iliyastb <skiliyas300@gmail.com>
2023-09-23 22:14:08 +0530

    update

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,13 +1,14 @@
+# data block to get the ami id
 data "aws_ami" "ami" {
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
 }
-
+# resource block to create the ec2 instance
 resource "aws_instance" "sample" {
   ami = data.aws_ami.ami.id
   instance_type = "t2.micro"
-  count = 2
+  count = 1
   tags = {
     Name = "sample"
   }
