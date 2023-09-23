38dcd8c3d763f11e62d00819f89906355eec1bed
iliyastb <skiliyas300@gmail.com>
2023-09-23 20:04:08 +0530

    update

--- 04-vars/vars.tf
+++ 04-vars/vars.tf
@@ -1,35 +1,36 @@
-## default variable
-#variable "sample" {
-#  default = "default variable"
-#}
-#output "sample" {
-#  value = var.sample
-#}
-#
-## tfvars - auto tfvars is the first priority
-#variable "tfvars" {}
-#
-#output "tfvars" {
-#  value = var.tfvars
-#}
-#
-## we give the variable input with command
-#variable "cli" {}
-#
-#output "cli" {
-#  value = var.cli
-#}
+# default variable
+variable "sample" {
+  default = "default variable"
+}
 
-# shell env variable, export TF_VAR_clitf="hello"
+output "sample" {
+  value = var.sample
+}
+
+# tfvars - auto tfvars is the first priority
+variable "tfvars" {}
+
+output "tfvars" {
+  value = var.tfvars
+}
+
+# we give the variable input with command
+variable "cli" {}
+
+output "cli" {
+  value = var.cli
+}
+
+# shell env variable, export TF_VAR_clitf="hello" or hello
 variable "clitf" {}
 
 output "clitf" {
   value = var.clitf
 }
 
-## cli asks for variable input
-#variable "input" {}
-#
-#output "input" {
-#  value = var.input
-#}
+# cli asks for variable input
+variable "input" {}
+
+output "input" {
+  value = var.input
+}
