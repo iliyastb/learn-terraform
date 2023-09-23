9e4c0b7c72f36ae5928151a28c3ba5f7ed9fec14
iliyastb <skiliyas300@gmail.com>
2023-09-23 21:10:45 +0530

    update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -2,17 +2,17 @@
 #resource "null_resource" "null" {
 #}
 
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = [860050401100]
-}
-
-#resource "aws_instance" "sample" {
-#  ami = data.aws_ami.ami.image_id
-#  instance_type = "t2.micro"
-#
-#  tags = {
-#    name = "sample"
-#  }
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex = "Centos-8-DevOps-Practice"
+#  owners = ["973714476881"]
 #}
+
+resource "aws_instance" "sample" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "sample"
+  }
+}
