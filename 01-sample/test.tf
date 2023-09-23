30544581cbee874b25b1cd703d48f5af133b313d
iliyastb <skiliyas300@gmail.com>
2023-09-23 20:56:45 +0530

    update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,3 +1,18 @@
 # This is the testing of the sample tf code
-resource "null_resource" "null" {
+#resource "null_resource" "null" {
+#}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = [860050401100]
+}
+
+resource "aws_instance" "sample" {
+  ami = data.aws_ami.ami.image_id
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "sample"
+  }
 }
