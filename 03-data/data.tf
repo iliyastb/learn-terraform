53eb8ef2d1b92ccc5007e405c90a71b8dc13a74a
iliyastb <skiliyas300@gmail.com>
2023-09-30 20:19:57 +0530

    Update

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,8 +1,10 @@
+# data block to get the ami id
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
+
 
 output "ami_id" {
   value = data.aws_ami.ami.image_id
--- 10-provisioner/main.tf
+++ 10-provisioner/main.tf
@@ -1,11 +1,11 @@
-# ami data block
+# data block to get the ami id
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
-# resource creation with provisioner
+# resource block to create the ec2 instance with provisioner
 resource "aws_instance" "test" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t2.micro"
@@ -15,7 +15,7 @@ resource "aws_instance" "test" {
     Name = "sample"
   }
 
-  # this is sub block of resource block
+  # this is sub block
   provisioner "remote-exec" {
 
     connection {
