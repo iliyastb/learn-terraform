00ed3435822e359f538cb9a7affb839c4cb9e73c
iliyastb <skiliyas300@gmail.com>
2023-09-30 19:50:12 +0530

    update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,2 +1,8 @@
 # This is the testing of the sample tf code
 resource "null_resource" "null" {}
+
+variable "security_group_id" {}
+
+data "aws_security_group" "selected" {
+  id = var.security_group_id
+}
