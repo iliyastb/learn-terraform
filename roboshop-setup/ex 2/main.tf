202b47ea0d3983ab19907b816b69877dc2730e93
iliyastb <skiliyas300@gmail.com>
2023-09-30 20:30:41 +0530

    Update

---
+++ roboshop-setup/ex 2/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "frontend" {
+  count                  = length(var.instances)
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0f1959ab92bc12167"]
+
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
+
+variable "instances" {
+  default = ["frontend", "catalogue", "user", "cart"]
+}
+
+output "ip" {
+  value = aws_instance.frontend.*.public_ip
+}
