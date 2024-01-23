af5afd4180e1416b5ade536c99f2103507d01ba6
iliyastb <skiliyas300@gmail.com>
2024-01-23 19:06:44 +0530

    Update

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -6,18 +6,18 @@ data "aws_ami" "ami" {
 }
 
 # resource block to create the ec2 instance, condition with string.
-resource "aws_instance" "sample" {
-  ami           = data.aws_ami.ami.id
-  instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
-
-  tags = {
-    Name = "sample"
-  }
-}
+#resource "aws_instance" "sample" {
+#  ami           = data.aws_ami.ami.id
+#  instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
+#
+#  tags = {
+#    Name = "sample"
+#  }
+#}
 
 variable "instance_type" {}
 
-# resource block to create the ec2 instance, condition with boolean.
+# resource block to create the ec2 instance, condition with boolean function.
 resource "aws_instance" "sample1" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
