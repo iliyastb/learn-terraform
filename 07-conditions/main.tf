8f33d14b742c869525b1179decc9a2af75eda4bb
iliyastb <skiliyas300@gmail.com>
2024-01-23 19:22:21 +0530

    Update

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -1,4 +1,4 @@
-# data block to get the ami id
+# data block to get the data of ami
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
@@ -6,18 +6,18 @@ data "aws_ami" "ami" {
 }
 
 # resource block to create the ec2 instance, condition with string.
-#resource "aws_instance" "sample" {
-#  ami           = data.aws_ami.ami.id
-#  instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
-#
-#  tags = {
-#    Name = "sample"
-#  }
-#}
+resource "aws_instance" "sample" {
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
+
+  tags = {
+    Name = "sample"
+  }
+}
 
 variable "instance_type" {}
 
-# resource block to create the ec2 instance, condition with boolean function.
+# resource block to create the ec2 instance, condition with string and boolean function.
 resource "aws_instance" "sample1" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
