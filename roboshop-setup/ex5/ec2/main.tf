6d84f69568f26fdb9f93645457af2a963fd508a4
iliyastb <skiliyas300@gmail.com>
2024-01-24 19:48:42 +0530

    Update

--- roboshop-setup/ex5/ec2/main.tf
+++ roboshop-setup/ex5/ec2/main.tf
@@ -4,10 +4,10 @@ data "aws_ami" "ami" {
   owners      = ["973714476881"]
 }
 
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instances" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = var.sg_id
 
   tags = {
     Name = var.component
