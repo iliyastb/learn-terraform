c290ab07efa0dba6a7ca57ef74e18ea2c618a58a
iliyastb <skiliyas300@gmail.com>
2024-02-06 18:26:54 +0530

    Update

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -1,1 +1,16 @@
-# nothing added
+terraform {
+  backend "s3" {
+    bucket = "robo-st"
+    key    = "05-s3-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test"
+  }
+}
