765b89729e790c95e70384900720efa0ba8744ae
iliyastb <skiliyas300@gmail.com>
2024-02-06 18:33:04 +0530

    Update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,2 +1,10 @@
 # This is the testing of the sample code
 resource "null_resource" "null" {}
+
+terraform {
+  backend "s3" {
+    bucket = "robo-st"
+    key    = "01-sample/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
