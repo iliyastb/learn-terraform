b9488d3143a9dd95be08ed45e375a16d7d64caed
iliyastb <skiliyas300@gmail.com>
2024-02-13 19:20:01 +0530

    Update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,2 +1,13 @@
 # This is the testing of the sample code
 resource "null_resource" "null" {}
+
+data "aws_instance" "ip" {
+  filter {
+    name   = "tag:Name"
+    values = ["frontend"]
+  }
+}
+
+output "ip" {
+  value = data.aws_instance.ip.public_ip
+}
