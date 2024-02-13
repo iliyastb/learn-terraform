88a12ed92f81271a8f7c0e1db770cf689579a2be
iliyastb <skiliyas300@gmail.com>
2024-02-13 19:23:23 +0530

    Update

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,6 +1,7 @@
 # This is the testing of the sample code
 resource "null_resource" "null" {}
 
+# This data block is used to get the single instance details
 data "aws_instance" "ip" {
   filter {
     name   = "tag:Name"
