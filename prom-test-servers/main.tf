eea2d8d6f29cdbdc5aa3fd79cd9279e56c3af7bf
iliyastb <skiliyas300@gmail.com>
2024-02-17 14:06:48 +0530

    Update

--- prom-test-servers/main.tf
+++ prom-test-servers/main.tf
@@ -7,6 +7,7 @@ resource "aws_spot_instance_request" "node1" {
     Name = "prom-server"
   }
 }
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-072aa986009d7d8af"
   instance_type = "t3.micro"
@@ -22,6 +23,7 @@ resource "aws_ec2_tag" "tag1" {
   key         = "Name"
   value       = "prom-server"
 }
+
 resource "aws_ec2_tag" "tag2" {
   resource_id = aws_spot_instance_request.node2
   key         = "Name"
