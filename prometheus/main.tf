13aef28933c4f339015ee56e2e458912b1ec4808
iliyastb <skiliyas300@gmail.com>
2024-02-17 18:46:05 +0530

    Update

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "prom" {
   ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0f1959ab92bc12167"]
   wait_for_fulfillment = true
 }
